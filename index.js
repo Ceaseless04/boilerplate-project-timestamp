542d474e11fe09bc29b312e212db22af545b40a5
Ceaseless04 <kristian120304@gmail.com>
2023-12-31 10:46:49 -0500

    completed the project for timestamp microservice

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 // where your node app starts
 
 // init project
+require("dotenv").config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { populate } = require("dotenv");
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,13 +20,39 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+function isInvalidDate(date) {
+  return date.toUTCString() === "Invalid Date";
+}
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api', function(req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
 });
 
+app.get('/api/:date', function (req, res) {
+  let date = new Date(req.params.date);
+  if(isInvalidDate(date)) {
+    date = new Date(req.params.date);
+  }
 
+  if(isInvalidDate(date)) {
+    res.json({
+      error: "Invalid Date"
+    });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get('api/:unix', function(req, res) {
+
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
