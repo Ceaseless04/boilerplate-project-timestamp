342e145e8359c923cf31881d5ac1c801b9dba477
Ceaseless04 <kristian120304@gmail.com>
2023-12-31 10:50:09 -0500

    completed the project for timestamp microservice

--- index.js
+++ index.js
@@ -50,10 +50,6 @@ app.get('/api/:date', function (req, res) {
   });
 });
 
-app.get('api/:unix', function(req, res) {
-
-});
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
